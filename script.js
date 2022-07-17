a7d03883cd9254e7144c479cefcfb25fed8e04c1
robgitnyc <robcellonyc@gmail.com>
2022-07-16 21:14:40 -0400

    .

--- script.js
+++ script.js
@@ -11,24 +11,34 @@ const playAgain = document.querySelector(".playAgain")
 const displayLives = document.querySelector(".lives")
 const displayScore = document.querySelector(".score")
 const highest = document.querySelector(".highest")
+const test = document.querySelector(".test");
+const buttonTest = document.querySelector(".buttonTest")
 
-let lives;
-let score = 0;
-let highestScore = 0;
 
 const newButton =document.createElement("button");
 newButton.setAttribute("type", "button"); 
 newButton.setAttribute("class", "newButton"); 
-newButton.textContent = "Play Again"
+newButton.textContent = "Play Again";
 
+let score = 0;
+if (localStorage.getItem("maxScore")===null) {
+    localStorage.setItem("maxScore", 0);
+}
 
 function colorBox() {
+    let maxScoreGlobal = localStorage.getItem("maxScore");
+    highest.textContent = "HIGHEST SCORE: "+ maxScoreGlobal;
+    let lives = 6;
+    colorToGuess.style.color = "white";
+    
+
+
+    
+
     display2.innerText = "";
-    lives = 4;
     displayScore.textContent = `your score: ${score}`;
     displayLives.textContent = `lives left: ${lives}`;
-    let storedHighest = localStorage.getItem('highestScore');
-    highest.textContent = `highest score: ${storedHighest}`;
+    
 
 
     const boxes = document.querySelectorAll(".box");  
@@ -36,56 +46,79 @@ function colorBox() {
 
     for (const box of boxes) {
 
-        box.addEventListener("click", youLose);
-
-        
+        box.addEventListener("click", minusLife);
+        box.addEventListener("click", () => {
+            
+           
+        });
         let color = randomColor();
         box.style.backgroundColor = color;
+        
 
-        console.log(window.getComputedStyle(box).getPropertyValue("background-color"));
     }
 
     let mysteryColor = (window.getComputedStyle(boxes[getRandomInt(0,boxes.length)]).getPropertyValue("background-color"));
     colorToGuess.textContent = mysteryColor;
     
-    function youLose() {
-        display2.innerText = "nope...";
-        display2.classList.remove("win")
 
-        display2.classList.add("lose");
+    function minusLife() {
+        display2.innerText = "nope...";
+        display2.style.color = "black";
+        setTimeout(()=>{
+            display2.style.color = "red";}, 100
+
+        )
+        
+
+     //   display2.classList.add("lose");
 
         lives--;
         displayLives.textContent = `lives left: ${lives}`;
 
-        if (lives === 0) {
+        if (lives === 0) { // you lose
             display2.innerText = "You lost. ";
             display2.appendChild(newButton);
-            display2.classList.remove("win")
+            display2.style.color = "white";
 
-            display2.classList.add("lose");
+     //       display2.classList.add("lose");
             displayScore.textContent = `your score: ${score}`;
-            
+            displayLives.textContent = ``;
+
+          
+
             score = 0;
 
             for (const box of boxes) {
-                box.removeEventListener("click", youLose);
+                box.removeEventListener("click", minusLife);
                 box.removeEventListener("click", youWin);
 
 
             }
         }
     }
-    function youWin() {
+    function youWin() { // plus point
 
-            display2.innerText = "...Yes! You WON. ";
+            display2.innerText = "That's right!";
             display2.appendChild(newButton);
-            display2.classList.remove("lose")
-            display2.classList.add("win");
+            display2.style.color = "white";
+            display2.style.color = mysteryColor;
+            colorToGuess.style.color = mysteryColor;
+        //    this.classList.add("flashBorder");
+            
             score++;
+            if (score > localStorage.getItem("maxScore")) {
+                localStorage.setItem("maxScore", score);
+            }
+            
+           let maxScore = localStorage.getItem("maxScore");
+           console.log(maxScore);
             displayScore.textContent = `your score: ${score}`;
+            displayLives.textContent = "";
+
+
 
             for (const box of boxes) {
-                box.removeEventListener("click", youLose);
+                box.removeEventListener("click", minusLife);
                 box.removeEventListener("click", youWin);
 
 
@@ -97,25 +130,21 @@ function colorBox() {
     for (const box of boxes) {
         if (window.getComputedStyle(box).getPropertyValue("background-color") === mysteryColor) {
             
-            box.removeEventListener("click", youLose);
+            box.removeEventListener("click", minusLife);
             box.addEventListener("click", youWin);
         } 
        
       
     }
 
-    if (score > highestScore) {
-        highestScore = score;
-    }
-
-    localStorage.setItem('highestScore',highestScore);
 
 
-
+    
 }
 
 
 function randomColor() {
+
     return `rgb(${getRandomInt(0,256)},${getRandomInt(0,256)},${getRandomInt(0,256)})`;
 }
 
@@ -130,3 +159,16 @@ colorBox();
 newButton.addEventListener("click", () => {
     colorBox();
 })
+
+// let storedTest;
+
+// buttonTest.addEventListener("click", () => {
+//     localStorage.setItem('testStorage', "lulululu");
+//     storedTest = localStorage.getItem("testStorage");
+//     test2();
+
+// })
+// test.textContent = localStorage.getItem("testStorage");
+
+
+
